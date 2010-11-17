eeb9f9913a5b1431c4379b663ec0512cecb6ccdd
webr3 <nathan@webr3.org>
2010-11-17 11:32:39 +0000

    build, compression etc

--- bin/merge.sh
+++ bin/merge.sh
@@ -1,1 +1,2 @@
 cat ../source/core.js ../source/rdfselector.js ../source/parsers.js ../source/serializers.js ../source/filters.js ../source/extensions.js ../source/hash.js ../source/compat.js ../source/loader.js > ../api.js
+java -jar ../../yui.jar ../api.js > ../api.min.js
